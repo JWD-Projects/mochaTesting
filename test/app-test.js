266f1b1bd47565ffaa87e93f02f5b235131890ff
Laize Ferraz <laizeferraz@gmail.com>
2021-06-20 21:13:54 +1000

    added one more exercise and a challenge

--- test/app-test.js
+++ test/app-test.js
@@ -8,15 +8,16 @@
    2 - should handle only one number as arguments
    3 - should be able to accept 0 as arguments
    4 - should return 0 if one argument is not a number
- */ 
-
-
-
-
-
-
+ */
 
 /*
   * Create unit tests for FizzBuzz
   1 - You should create an unit test for each functionality of the FizzBuzz function
 */
+
+/* Create a unit test for the gatherNamesOf function.
+  1- It should should return an array
+  2- It should give me the same output length as the input
+  3- It should give me the property name value of the object passed in
+  * CHALLENGE: use beforeEach method to write a more concise test code.
+ */
